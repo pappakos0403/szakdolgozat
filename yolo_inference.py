ef01308f4993cdf904f8d5a8769eb05cb686bdea
pappakos0403 <pappakos0403@gmail.com>
2025-03-11 15:25:07 +0100

    utils init

--- yolo_inference.py
+++ yolo_inference.py
@@ -2,7 +2,7 @@ from ultralytics import YOLO
  
 model = YOLO('./best.pt')
 
-results = model.predict('input_videos/VID_20240513_101917.mp4', save=True, project='output', name='yolo_results')
+results = model.predict('input_videos/08fd33_4.mp4', save=True, project='output', name='yolo_results')
 print(results[0])
 print('=============')
 for box in results[0].boxes:
